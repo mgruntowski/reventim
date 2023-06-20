201602004878aa5dca3ccc04658cc90d0f04283d
Mateus Gruntowski Mendes <mateusgmendes@hotmail.com>
2023-06-20 19:47:35 -0300

    handle already exists user error

--- pages/api/user.ts
+++ pages/api/user.ts
@@ -1,15 +1,23 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { Prisma } from "@prisma/client";
 
 import prisma from "lib/prisma";
 
 const handle = async (req: NextApiRequest, res: NextApiResponse) => {
   const data = req.body;
 
-  const result = await prisma.user.create({
-    data: { ...data },
-  });
-
-  res.json(result);
+  try {
+    const result = await prisma.user.create({
+      data: { ...data },
+    });
+    res.json(result);
+  } catch (err) {
+    if (err instanceof Prisma.PrismaClientKnownRequestError) {
+      if (err.code === "P2002") {
+        res.status(409).json({ message: "Usuário já cadastrado." });
+      }
+    }
+  }
 };
 
 export default handle;
