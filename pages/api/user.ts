e191e140c9a9a79e8568b671b0197da7c8c691b1
Mateus Gruntowski Mendes <mateusgmendes@hotmail.com>
2023-06-20 20:08:12 -0300

    remove logs

--- pages/api/user.ts
+++ pages/api/user.ts
@@ -11,25 +11,13 @@ const handle = async (req: NextApiRequest, res: NextApiResponse) => {
       data: { ...data },
     });
 
-    console.log("tentou enviar aqui");
-
     res.status(200);
   } catch (err) {
-    console.log(
-      "instance ->",
-      err instanceof Prisma.PrismaClientKnownRequestError
-    );
-    console.log("code ->", err.code);
-    console.log("code compare ->", err.code === "P2002");
-
     if (err instanceof Prisma.PrismaClientKnownRequestError) {
-      console.log("primeiro if");
       if (err.code === "P2002") {
-        console.log("deveria retornar aqui");
         res.status(409).json({ message: "Usuário já cadastrado." });
       }
     } else {
-      console.log("ultimo console log");
       res.json(err);
     }
   }
